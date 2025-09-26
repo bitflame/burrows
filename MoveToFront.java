b90d53b5f39a86ca0984f30dcc3a6b8a1582d3f6
BitFlame <monazzam@protonmail.com>
2025-09-26 10:53:05 -0400

    Saving the latest

--- MoveToFront.java
+++ MoveToFront.java
@@ -12,39 +12,37 @@ import java.util.List;
 
 public class MoveToFront {
     private static final int R = 256;
-    private static char[] _chars;
     private static List<Character> stringChars;
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-        _chars = new char[R];
-        for (int i = 0; i < R; i++) {
-            _chars[i] = (char) i;
-        }
         stringChars = new ArrayList<>();
-        // System.out.println("Encoding. Here is what is in the array before endoding.");
-        // printString();
         int count = 0;
         while (!BinaryStdIn.isEmpty()) {
             count++;
             char currentChar = BinaryStdIn.readChar();
-            for (int i = currentChar; i > 0; i--) {
-                _chars[i] = _chars[i - 1];
+            if (stringChars.contains(currentChar)) {
+                BinaryStdOut.write(charIndex(currentChar));
             }
-            _chars[0] = currentChar;
+            else {
+                BinaryStdOut.write(0);
+            }
+            stringChars.add(currentChar);
         }
-        // System.out.println("Here is what is in the array after the string is entered.");
-        // printString();
-        // todo - need to get clarification on where move to front is happening. It is doing what I expect and I get the result I want from Hexdump so far
-        for (int i = 0; i < count; i++) BinaryStdOut.write(_chars[i]);
         BinaryStdOut.close();
     }
 
-    private static void printString() {
-        for (int i = 0; i < R; i++) {
-            // System.out.println(_chars[i]);
-            BinaryStdOut.write(_chars[i]);
+    private static int charIndex(char currentChar) {
+        int index = 0;
+        for (char c : stringChars) {
+            if (c == currentChar) return index;
+            index++;
         }
+        return -1;
+    }
+
+    private static void printString() {
+        for (char c : stringChars) System.out.println(" " + c + " ");
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
@@ -52,7 +50,6 @@ public class MoveToFront {
         System.out.println("Decoding.");
         for (char c : stringChars) {
             for (int i = c; i > 0; i--) {
-                _chars[c] = _chars[c - 1];
             }
         }
     }
