b1cff5830dedd3c1114472990765317f28bcbe83
BitFlame <monazzam@protonmail.com>
2025-09-27 16:30:48 -0400

    Saving the latest

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -10,20 +10,26 @@ import java.util.Comparator;
 public class CircularSuffixArray {
     char[][] suffixes;
     int N;
+    int[] indices;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
+        indices = new int[N];
         N = s.length();
-        suffixes = new char[N][N];
+        suffixes = new char[N][N + 1];
         char[] chars = s.toCharArray();
-        int index = 0;
-        suffixes[0] = new char[N];
+        int index = 1, arrayIndex = 0;
+        suffixes[0] = new char[N + 1];
+        suffixes[0][0] = 0;
         for (char c : chars) {
             suffixes[0][index++] = c;
         }
+        arrayIndex = 1;
         for (int i = 1; i < N; i++) {
             suffixes[i] = new char[N];
-            index = 0;
+            index = 1;
+            suffixes[i][0] = (char) arrayIndex;
+            arrayIndex++;
             for (char c : shiftArry(chars, 1)) {
                 suffixes[i][index] = c;
                 index++;
@@ -39,7 +45,7 @@ public class CircularSuffixArray {
         System.out.println("After Sorting:");
         Arrays.sort(suffixes, new Comparator<char[]>() {
             public int compare(char[] o1, char[] o2) {
-                for (int i = 0; i < N; i++) {
+                for (int i = 1; i < N + 1; i++) {
                     if (o1[i] > o2[i]) return 1;
                     if (o2[i] > o1[i]) return -1;
                 }
@@ -57,13 +63,13 @@ public class CircularSuffixArray {
     private char[] shiftArry(char[] chars, int shiftDigits) {
         // char[] chars = s.toCharArray();
         char[] cache = new char[shiftDigits];
-        int charPointer = 0;
-        while (charPointer < shiftDigits) {
-            cache[charPointer] = chars[charPointer];
+        int charPointer = 1;
+        while (charPointer <= shiftDigits) {
+            cache[charPointer - 1] = chars[charPointer];
             charPointer++;
         }
-        for (int i = shiftDigits - 1; i < chars.length - 1; i++) {
-            chars[i] = chars[i + shiftDigits];
+        for (int i = shiftDigits + 1; i < chars.length - 1; i++) {
+            chars[i] = chars[i + shiftDigits + 1];
         }
         charPointer = chars.length - shiftDigits;
         for (int i = 0; i < shiftDigits; i++) {
@@ -74,7 +80,7 @@ public class CircularSuffixArray {
 
     // length of s
     public int length() {
-        return -1; // for now
+        return N; // for now
     }
 
     // returns index of ith sorted suffix
