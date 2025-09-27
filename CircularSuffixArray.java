dcb5e9d3d7466fc306e752c45c909461ad441e82
BitFlame <monazzam@protonmail.com>
2025-09-27 16:55:05 -0400

    Saving the latest

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -20,15 +20,15 @@ public class CircularSuffixArray {
         char[] chars = s.toCharArray();
         int index = 1, arrayIndex = 0;
         suffixes[0] = new char[N + 1];
-        suffixes[0][0] = 0;
+        suffixes[0][0] = '0';
         for (char c : chars) {
             suffixes[0][index++] = c;
         }
         arrayIndex = 1;
         for (int i = 1; i < N; i++) {
-            suffixes[i] = new char[N];
+            suffixes[i] = new char[N + 1];
             index = 1;
-            suffixes[i][0] = (char) arrayIndex;
+            suffixes[i][0] = (char) index;
             arrayIndex++;
             for (char c : shiftArry(chars, 1)) {
                 suffixes[i][index] = c;
@@ -63,13 +63,13 @@ public class CircularSuffixArray {
     private char[] shiftArry(char[] chars, int shiftDigits) {
         // char[] chars = s.toCharArray();
         char[] cache = new char[shiftDigits];
-        int charPointer = 1;
-        while (charPointer <= shiftDigits) {
-            cache[charPointer - 1] = chars[charPointer];
+        int charPointer = 0;
+        while (charPointer < shiftDigits) {
+            cache[charPointer] = chars[charPointer];
             charPointer++;
         }
-        for (int i = shiftDigits + 1; i < chars.length - 1; i++) {
-            chars[i] = chars[i + shiftDigits + 1];
+        for (int i = shiftDigits; i < chars.length - 1; i++) {
+            chars[i] = chars[i + shiftDigits];
         }
         charPointer = chars.length - shiftDigits;
         for (int i = 0; i < shiftDigits; i++) {
