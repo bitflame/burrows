3e53b23e1234ce35e54ab507ce3d824edeaed48e
BitFlame <monazzam@protonmail.com>
2025-09-28 11:59:45 -0400

    Saving the latest. Circular suffix seems to be working.

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -19,25 +19,23 @@ public class CircularSuffixArray {
         suffixes = new char[N][N + 1];
         char[] chars = s.toCharArray();
         int index = 1, arrayIndex = 0;
-        suffixes[0] = new char[N + 1];
-        suffixes[0][0] = '0';
+        suffixes[0][0] = 0;
         for (char c : chars) {
             suffixes[0][index++] = c;
         }
         arrayIndex = 1;
         for (int i = 1; i < N; i++) {
-            suffixes[i] = new char[N + 1];
             index = 1;
-            suffixes[i][0] = (char) index;
+            suffixes[i][0] = (char) arrayIndex;
             arrayIndex++;
-            for (char c : shiftArry(chars, 1)) {
+            for (char c : shiftArry(chars)) {
                 suffixes[i][index] = c;
                 index++;
             }
         }
         System.out.println("Before Sorting: ");
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j < N + 1; j++) {
                 System.out.printf("%c ", suffixes[i][j]);
             }
             System.out.println();
@@ -45,7 +43,7 @@ public class CircularSuffixArray {
         System.out.println("After Sorting:");
         Arrays.sort(suffixes, new Comparator<char[]>() {
             public int compare(char[] o1, char[] o2) {
-                for (int i = 1; i < N + 1; i++) {
+                for (int i = 1; i <= N; i++) {
                     if (o1[i] > o2[i]) return 1;
                     if (o2[i] > o1[i]) return -1;
                 }
@@ -53,28 +51,20 @@ public class CircularSuffixArray {
             }
         });
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j <= N; j++) {
                 System.out.printf("%c ", suffixes[i][j]);
             }
             System.out.println();
         }
     }
 
-    private char[] shiftArry(char[] chars, int shiftDigits) {
+    private char[] shiftArry(char[] chars) {
         // char[] chars = s.toCharArray();
-        char[] cache = new char[shiftDigits];
-        int charPointer = 0;
-        while (charPointer < shiftDigits) {
-            cache[charPointer] = chars[charPointer];
-            charPointer++;
+        char temp = chars[0];
+        for (int i = 0; i < N - 1; i++) {
+            chars[i] = chars[i + 1];
         }
-        for (int i = shiftDigits; i < chars.length - 1; i++) {
-            chars[i] = chars[i + shiftDigits];
-        }
-        charPointer = chars.length - shiftDigits;
-        for (int i = 0; i < shiftDigits; i++) {
-            chars[charPointer] = cache[i];
-        }
+        chars[N - 1] = temp;
         return chars;
     }
 
