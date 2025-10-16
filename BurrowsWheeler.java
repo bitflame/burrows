faacd636d886eba270c6134f076228f341c6679f
BitFlame <monazzam@protonmail.com>
2025-10-16 12:13:54 -0400

    Saving the latest.

--- BurrowsWheeler.java
+++ BurrowsWheeler.java
@@ -32,33 +32,15 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
-        t = "ARD!RCAAAABB".toCharArray();
-        N = t.length;
-        count = new int[R + 1];
-        aux = new char[N];
-        for (int i = 0; i < N; i++) {
-            count[t[i] + 1]++;
-        }
-        for (int r = 0; r < R; r++) {
-            count[r + 1] += count[r];
-        }
-        System.out.println("");
-        for (int i = 0; i < N; i++) {
-            aux[count[t[i]]++] = t[i];
-        }
-        for (int i = 0; i < N; i++) {
-            t[i] = aux[i];
-        }
-        // for (char c : t) {
-        //     System.out.printf("%c ", c);
-        // }
-        // System.out.println();
+        // read in an integer and a string, loop through the string and set
+        // next[] to the index of str.chartAt(0) == ! i.e. the string that
+        // starts with the character you are looking at - The first instance of it
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
         if (args[0].equals("-")) transform();
-        // inverseTransform();
+        if (args[0].equals("+")) inverseTransform();
     }
 }
