08a7295d361444adf17cc35567b3d4d568a0e5a1
BitFlame <monazzam@protonmail.com>
2025-10-19 19:27:59 -0400

    Fixed a number of bugs and an error for the staring BANANA. Is sorts fine now.

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -7,15 +7,16 @@ public class CircularSuffixArray {
     private static int N;
     private static int offset, countFrequenciesCounter;
     private static int[] countFrequencies;
-    private static String s;
     private static final int RADIX_SORT_CUTOFF = 12;
     private static int d = 0;
+    private static String s;
 
-    public CircularSuffixArray(String s) {
-        if (s == null) throw new IllegalArgumentException(
-                "The strings passed to this method should not be null.");
+    public CircularSuffixArray(String str) {
+        if (str == null)
+            throw new IllegalArgumentException(
+                    "The strings passed to this method should not be null.");
         countFrequenciesCounter = 0;
-        s = s;
+        s = str;
         N = s.length();
         suffixes = new CircularSuffix[s.length()];
         for (int i = 0; i < N; i++) {
@@ -25,10 +26,10 @@ public class CircularSuffixArray {
         offset = 0; // tracks the columns of common characters between lo and hi
         aux = new int[N];
         countFrequencies = new int[N];
-        int lo = 0, hi = N
-                - 1; // d: column being considered from lo: the first string to hi: the last string
+        int lo = 0, hi = N - 1;
+        // d: column being considered from lo: the first string to hi: the last string
         // System.out.printf(
-        //         "line 29 messareturn super.toString();ge: lo: %d, hi: %d d: %d offset: %d \n", lo,
+        //         "line 31 messareturn super.toString();ge: lo: %d, hi: %d d: %d offset: %d \n", lo,
         //         hi, d, offset);
         sort(suffixes, lo, hi, d, 0);
         hi = countFrequencies[0] - 1;
@@ -37,7 +38,8 @@ public class CircularSuffixArray {
             lo = lo + countFrequencies[generalCounter];
             hi = lo + countFrequencies[generalCounter + 1] - 1;
             generalCounter++;
-            if (hi > lo) sortMatchingStrings(lo, hi, d);
+            if (hi > lo)
+                sortMatchingStrings(lo, hi, d);
         }
     }
 
@@ -50,10 +52,12 @@ public class CircularSuffixArray {
     }
 
     private void sortMatchingStrings(int lo, int hi, int d) {
-        // returns the values for offset, matchingLo, matchingHi, and count of the region with matching characters
+        // returns the values for offset, matchingLo, matchingHi, and count of the
+        // region with matching characters
         // for the current column referred to by d
         int startOfMatch = lo, endOfMatch = lo, currentIndex = lo, localOffset
-                = 0; // matching hi starts at lo and increases
+                = 0; // matching hi starts at lo and
+        // increascountFrequencieses
         offset = 0;
         boolean foundRowsWithMatchingCharacters = true;
         currentIndex = lo;
@@ -61,45 +65,50 @@ public class CircularSuffixArray {
             foundRowsWithMatchingCharacters = false;
             while (currentIndex < hi) {
                 if (currentIndex <= hi
-                        && suffixes[currentIndex].charAt(offset + d) == suffixes[
-                        currentIndex + 1].charAt(offset + d)) {
+                        && suffixes[currentIndex].charAt(offset + d) == suffixes[currentIndex + 1]
+                        .charAt(offset + d)) {
                     startOfMatch = currentIndex;
                     foundRowsWithMatchingCharacters = true;
                     while (currentIndex < hi
-                            && suffixes[currentIndex].charAt(offset + d) == suffixes[
-                            currentIndex + 1].charAt(offset + d)) {
+                            && suffixes[currentIndex].charAt(offset + d) == suffixes[currentIndex
+                            + 1]
+                            .charAt(offset + d)) {
                         currentIndex++;
                     }
                     localOffset = offset + 1;
                     endOfMatch = currentIndex;
                     int temp = startOfMatch;
-                    // can I add another col? temp is just a temporary variable poiting to each line instead of currentIndex
-                    while (suffixes[temp].charAt(d + localOffset) == suffixes[
-                            temp + 1].charAt(d + localOffset) && temp <= endOfMatch) {
+                    // can I add another col? temp is just a temporary variable poiting to each line
+                    // instead of currentIndex
+                    while (suffixes[temp].charAt(d + localOffset) == suffixes[temp + 1]
+                            .charAt(d + localOffset) && temp <= endOfMatch) {
                         while (temp < endOfMatch
                                 && suffixes[temp].charAt(d + localOffset) == suffixes[temp
                                 + 1].charAt(d + localOffset)) {
                             temp++;
                         }
+
                         if (temp == endOfMatch
                                 && suffixes[startOfMatch].charAt(d + localOffset) == suffixes[
-                                startOfMatch + 1].charAt(d + localOffset)) {
+                                startOfMatch + 1]
+                                .charAt(d + localOffset)) {
                             temp = startOfMatch;
                             localOffset++;
                         }
+
                     }
                     offset++;
-                    if (localOffset > offset) offset = localOffset;
+                    if (localOffset > offset)
+                        offset = localOffset;
                     if (startOfMatch - endOfMatch > RADIX_SORT_CUTOFF) {
-                        sort(suffixes, startOfMatch, endOfMatch, offset, localOffset);
+                        sort(suffixes, startOfMatch, endOfMatch, d, offset);
                         foundRowsWithMatchingCharacters = false;
                     }
                     else {
-                        insertionSortForCircSuffixArray(suffixes, startOfMatch, endOfMatch, offset,
-                                                        localOffset);
+                        insertionSortForCircSuffixArray(suffixes, startOfMatch, endOfMatch, d,
+                                                        offset);
                         foundRowsWithMatchingCharacters = false;
                     }
-
                 }
                 currentIndex++;
             }
@@ -116,9 +125,10 @@ public class CircularSuffixArray {
             }
         }
         // System.out.printf(
-        //         "Line 90 message - hi: %d lo: %d d: %d offset: %d After Sorting in Insertion sort: \n",
-        //         hi, lo, d, offset);
+        //         "Line 126 message - lo: %d hi: %d d: %d offset: %d After Sorting in Insertion sort: \n",
+        //         lo, hi, d, offset);
         // for (int i = 0; i < a.length; i++) {
+        //     System.out.printf("%d ", a[i].start);
         //     for (int j = 0; j < a.length; j++) {
         //         System.out.print((char) a[i].charAt(j));
         //     }
@@ -153,8 +163,9 @@ public class CircularSuffixArray {
 
     // returns the index of ith sorted suffix
     public int index(int i) {
-        if (i < 0 || i > N) throw new IllegalArgumentException(
-                "The suffix index should be within the string lenght it refers to.");
+        if (i < 0 || i > N)
+            throw new IllegalArgumentException(
+                    "The suffix index should be within the string lenght it refers to.");
         return suffixes[i].start;
     }
 
@@ -196,11 +207,13 @@ public class CircularSuffixArray {
             a[i].start = aux[i - lo];
         }
         // updateOffset(lo, hi, d);
-        // System.out.printf("Line 155 message - hi: %d lo: %d d: %d offset: %d After Sorting: \n", hi,
-        //                   lo, d, offset);
+        // System.out.printf("Line 210 message - hi: %d lo: %d d: %d offset: %d After Sorting: \n", hi,
+        //         lo, d, offset);
         // for (int i = 0; i < a.length; i++) {
+        //     System.out.printf("Start of this suffix: %d ", a[i].start);
         //     for (int j = 0; j < a.length; j++) {
         //         System.out.print((char) a[i].charAt(j));
+
         //     }
         //     System.out.println();
         // }
@@ -209,10 +222,30 @@ public class CircularSuffixArray {
     }
 
     public static void main(String[] args) {
+        System.out.println("Processing String 1");
+        String shortString = "ABC";
+        CircularSuffixArray csaShort = new CircularSuffixArray(shortString);
+        for (int i = 0; i < shortString.length(); i++) {
+            System.out.println(csaShort.index(i));
+        }
+        System.out.println("Processing String 2");
         String str = "ABRACADABRA!";
-        int N = str.length();
+        int N = str.length();// change str to ARD!RCAAAABB and see if it sorted
         CircularSuffixArray circularSuffixArray = new CircularSuffixArray(str);
         System.out.println(circularSuffixArray.length());
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(circularSuffixArray.index(i));
+        }
+
+        System.out.println("Processing String 3");
+        str = "BANANA";
+        circularSuffixArray = new CircularSuffixArray(str);
+        System.out.println(circularSuffixArray.index(0));
+        System.out.println();
+        System.out.println("Processing String 4");
+        str = "ARD!RCAAAABB";
+        circularSuffixArray = new CircularSuffixArray(str);
     }
 }
 
